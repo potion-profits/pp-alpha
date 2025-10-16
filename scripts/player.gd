471097da3c4bc40f69b7bbe3eea836d44bca851b
J-Dees <jdees@outlook.com>
2025-10-16 15:57:39 -0700

    add sprint and interact button

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,7 +4,6 @@ var velocity
 const SPEED = 300
 @onready var animated_sprite = $AnimatedSprite2D
 
-
 func _physics_process(delta):
 	# allow variable screen sizes
 	var screen_size = get_viewport_rect().size
@@ -14,21 +13,31 @@ func _physics_process(delta):
 	# flips y direction to neg or positive based on keypress input
 	var y_dir = Input.get_axis("move_up", "move_down")
 	if y_dir:
-		velocity.y = y_dir * SPEED * delta
+		velocity.y = y_dir
 	
 	# flips x direction to neg or positive based on keypress input
 	var x_dir = Input.get_axis("move_left", "move_right")
 	if x_dir:
-		velocity.x = x_dir * SPEED * delta
+		velocity.x = x_dir
 		if x_dir > 0:
 			animated_sprite.flip_h = false
 		else:
 			animated_sprite.flip_h = true
 	
+	velocity = velocity.normalized() * SPEED * delta
+	
+	var sprint = Input.is_action_pressed("sprint")
+	
 	if x_dir or y_dir:
 		animated_sprite.play("walk")
 	else:
 		animated_sprite.play("default")
+		
+	if sprint and (x_dir or y_dir):
+		animated_sprite.speed_scale = 2.0
+		velocity = velocity * 2
+	else:
+		animated_sprite.speed_scale = 1.0
 	
 	move_and_collide(velocity)
 	
