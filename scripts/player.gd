265251608a01bc5b821f4203443da7b6423bdfb0
J-Dees <jdees@outlook.com>
2025-10-16 07:30:41 -0700

    upload placeholder assets, imrpove player aesthetic

---
+++ scripts/player.gd
@@ -0,0 +1,54 @@
+extends PhysicsBody2D
+
+var velocity
+const SPEED = 300
+@onready var animated_sprite = $AnimatedSprite2D
+
+
+func _physics_process(delta):
+	# allow variable screen sizes
+	var screen_size = get_viewport_rect().size
+	# reset speed each tick
+	velocity = Vector2(0,0)
+	
+	# flips y direction to neg or positive based on keypress input
+	var y_dir = Input.get_axis("move_up", "move_down")
+	if y_dir:
+		velocity.y = y_dir * SPEED * delta
+	
+	# flips x direction to neg or positive based on keypress input
+	var x_dir = Input.get_axis("move_left", "move_right")
+	if x_dir:
+		velocity.x = x_dir * SPEED * delta
+		if x_dir > 0:
+			animated_sprite.flip_h = false
+		else:
+			animated_sprite.flip_h = true
+	
+	if x_dir or y_dir:
+		animated_sprite.play("walk")
+	else:
+		animated_sprite.play("default")
+	
+	move_and_collide(velocity)
+	
+	# locks character to rectangle from (0,0) to (sreen_size.x, screen_size.y)
+	global_position = global_position.clamp(Vector2(0,0), screen_size)
+	
+	"""
+	if collision_info:
+		velocity = Vector2(0,0)
+	if global_position.x < 0:
+		global_position.x = 0
+	if global_position.y < 0:
+		global_position.y = 0
+	if global_position.y > screen_size.y:
+		global_position.y = screen_size.y
+	if global_position.x > screen_size.x:
+		global_position.x = screen_size.x
+	-------------------------------------------  ^
+	or we can use clamp / clampf() ______________|
+	-------------------------------------------
+	global_position.x = clampf(global_position.x, 0, screen_size.x)
+	global_position.y = clampf(global_position.y, 0, screen_size.y)
+	"""
