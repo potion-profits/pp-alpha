c9210524e8733245c6b1ba471f7efc325a88998a
jmgdls <jazzymae.delossantos@gmail.com>
2025-10-27 23:11:25 -0700

    Implement 8-directional movement and animation frames for Chay. Chay now spawns in center of screen.

--- shop/scripts/interactable_cauldron.gd
+++ shop/scripts/interactable_cauldron.gd
@@ -11,32 +11,26 @@ var count:int = 0
 
 @onready var animated_sprite: AnimatedSprite2D = get_parent().get_node("AnimatedSprite2D")
 
-#lets player collect if in the area
+#lets player interact if in the area
 func _input(event: InputEvent) -> void:
-	var count: int = 0
 	if player_in_area == true:	
 		if event.is_action_pressed("interact"):
 			animation_play()
 
 #tracks when player is in area and sets the player
 func _on_interactable_area_body_entered(body: Node) -> void:
-	print("Enter")
 	if body.has_method("player"):
 		player = body
 		player_in_area = true
 
 #tracks when player leaves area
 func _on_interactable_area_body_exited(body : Node) -> void:
-	print("Exit")
 	if body.has_method("player"):
 		player_in_area = false
 		
 #on interact, animation plays on sprite
-var animation_cycle_time: float = 1.5
-
 func animation_play() -> void:
 	if animated_sprite and animated_sprite.sprite_frames.has_animation(animation_name):
 		animated_sprite.play(animation_name)
-		print("Playing animation: " + animation_name)
 	else:
 		push_error("AnimatedSprite2D or animation '" + animation_name + "' not found!")
