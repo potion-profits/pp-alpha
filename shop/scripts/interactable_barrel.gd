e5b09d1fc27aa859528eccd4b3dcd7f181caa0f0
elijah-villanu <elijahred9@outlook.com>
2025-10-28 19:20:38 -0700

    Create Barrel Scene and Modify Interaction Scripts
    Fix interaction scripts: Instead of the interactable areas checking if the PhysicsBody2D is the player, it checks the body name to be "Player"
    
    Create Barrel Scene

--- shop/scripts/interactable_barrel.gd
+++ shop/scripts/interactable_barrel.gd
@@ -1,7 +1,5 @@
 extends Area2D
 
-@export var animation_name: String = "default"  # Name of animation to play
-
 #used to check the player that can interact
 var player: PhysicsBody2D = null
 #tracks if player can interact
@@ -9,13 +7,11 @@ var player_in_area:bool = false
 
 var count:int = 0
 
-@onready var animated_sprite: AnimatedSprite2D = get_parent().get_node("CauldronAnim")
-
 #lets player interact if in the area
 func _input(event: InputEvent) -> void:
 	if player_in_area == true:	
 		if event.is_action_pressed("interact"):
-			animation_play()
+			print("Hello")
 
 #tracks when player is in area and sets the player
 func _on_interactable_area_body_entered(body: Node) -> void:
@@ -25,12 +21,5 @@ func _on_interactable_area_body_entered(body: Node) -> void:
 
 #tracks when player leaves area
 func _on_interactable_area_body_exited(body : Node) -> void:
-	if body.name == "Player":
+	if body.has_method("player"):
 		player_in_area = false
-		
-#on interact, animation plays on sprite
-func animation_play() -> void:
-	if animated_sprite and animated_sprite.sprite_frames.has_animation(animation_name):
-		animated_sprite.play(animation_name)
-	else:
-		push_error("AnimatedSprite2D or animation '" + animation_name + "' not found!")
