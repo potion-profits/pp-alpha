1e765d6a04ccf1b8e7b2ec1a5e6a5709954a60fe
elijah-villanu <elijahred9@outlook.com>
2025-10-29 17:17:49 -0700

    Merge branch 'main' into basic_level_interactions

---
+++ scenes/ui/pause_menu.gd
@@ -0,0 +1,18 @@
+extends "res://scenes/ui/base_menu.gd"
+
+
+func _ready()->void:
+	button_map = {
+		"MarginContainer/VBoxContainer/Resume": "res://assets/ui/play_button.tres",
+		"MarginContainer/VBoxContainer/Menu": "res://assets/ui/menu_button.tres"
+	}
+	super._ready()
+
+
+func _on_menu_pressed()->void:
+	#save and return to menu!! for now just menu
+	get_tree().change_scene_to_file("res://scenes/ui/start_menu.tscn")
+
+
+func _on_resume_pressed()->void:
+	GameManager.unpause()
---
+++ scenes/ui/start_menu.gd
@@ -0,0 +1,19 @@
+extends "res://scenes/ui/base_menu.gd"
+
+func _ready()->void:
+	button_map = {
+		"MarginContainer/VBoxContainer/Play": "res://assets/ui/play_button.tres",
+		"MarginContainer/VBoxContainer/Options": "res://assets/ui/options_button.tres",
+		"MarginContainer/VBoxContainer/Quit": "res://assets/ui/quit_button.tres"
+	}
+	super._ready()
+
+
+func _on_play_pressed()->void:
+	get_tree().change_scene_to_file("res://scenes/playground/playground.tscn")
+
+func _on_options_pressed()->void:
+	get_tree().change_scene_to_file("res://scenes/ui/options_menu.tscn")
+
+func _on_quit_pressed()->void:
+	get_tree().quit()
