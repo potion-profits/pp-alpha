1e58977e4246211f2950e84a2e23d0eb096bcb81
Ozcar Cayetano <olopez2015.ol@gmail.com>
2025-11-04 15:28:06 -0800

    adjusted database script

--- globals/SQLTutorial.gd
+++ globals/SQLTutorial.gd
@@ -1,14 +1,10 @@
-extends Control
+extends Node
 
 var database: SQLite
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	database = SQLite.new()
-	database.path="res://data.db"
+	database.path="res://globals/data.db"
 	database.open_db()
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	database.close_db()
+	
