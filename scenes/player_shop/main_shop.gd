5419d58d7a7822ea376329c033c18e5c18a8e16f
Connor-Villanueva <ckvillan@calpoly.edu>
2025-11-04 14:03:14 -0800

    Basic Shop Layout

---
+++ scenes/player_shop/main_shop.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _ready()->void:
+	var pause_scene : Resource = preload("res://scenes/ui/pause_menu.tscn")
+	var menu_instance : Node = pause_scene.instantiate()
+	add_child(menu_instance)
+	GameManager.set_pause_menu(menu_instance.get_node("PauseMenuControl"))
