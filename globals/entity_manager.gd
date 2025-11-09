3ed96ff909ca11aeda13f1376e1635606b7e55fc
elijah-villanu <elijahred9@outlook.com>
2025-11-08 16:02:26 -0800

    Revert "Merge branch 'inventory' into basic_level_interactions"
    This reverts commit 5971223285066cb8cc38bbd4f8ca92530fd898a5, reversing
    changes made to e3353adc4b38c26045634e8d1d9ecfbf5be9b547.

--- globals/entity_manager.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-class_name EntityManager
-
-func load_from_dict(data:Dictionary)->void:
-	var entity_scene : PackedScene = load(data["scene_uid"])
-	var entity: Entity = entity_scene.instantiate() as Entity
-	entity.from_dict(data)
-	add_child(entity)
-		
