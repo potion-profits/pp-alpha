ff5662a9b00d99630461c4cebb0b4f3c40466125
OzcarC <olopez2015.ol@gmail.com>
2025-11-10 02:04:41 -0800

    simple ui icons to distinguish mixed, sellable potions

--- scenes/inventory/inv_item_stack_ui.gd
+++ scenes/inventory/inv_item_stack_ui.gd
@@ -3,6 +3,8 @@ class_name ItemStackUI
 
 @onready var item_visuals : Sprite2D = $item_display
 @onready var amount_text: Label = $item_amount
+@onready var sellable_label: Label = $sellable_label
+@onready var mixable_label: Label = $mixable_label
 
 var invSlot : InvSlot
 
@@ -10,7 +12,9 @@ func update_slot()->void:
 	if !invSlot or !invSlot.item:
 		item_visuals.visible = false
 		amount_text.visible = false
-	
+		sellable_label.visible = false
+		mixable_label.visible = false
+		
 	else:
 		item_visuals.visible = true
 		item_visuals.texture = invSlot.item.texture
@@ -19,3 +23,6 @@ func update_slot()->void:
 		if invSlot.amount <=1:
 			amount_text.visible = false
 		amount_text.text = str(invSlot.amount)
+		if invSlot.item:
+			sellable_label.visible = invSlot.item.sellable
+			mixable_label.visible = invSlot.item.mixable
