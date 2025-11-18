e663b5bd83f859009448a9c30ebfee390a29e598
J-Dees <jdees@outlook.com>
2025-11-18 12:04:36 -0800

    npcs move around shop

--- scenes/npc_alt/basic_npc.gd
+++ scenes/npc_alt/basic_npc.gd
@@ -26,6 +26,8 @@ var shelves : Array
 var target : Vector2i
 var checkout : Vector2i
 
+signal npc_left
+
 func _ready() -> void:
 	var color : int = randi_range(0,TYPES.size() - 1)
 	sprite.modulate = TYPES[color]
@@ -33,6 +35,7 @@ func _ready() -> void:
 func _physics_process(_delta : float) -> void:
 	velocity = Vector2.ZERO
 	if current_path.is_empty():
+		npc_action()
 		return
 	var local_target : Vector2 = floor_map.tilemap.map_to_local(current_path[path_index])
 	var direction : Vector2 = (local_target - global_position).normalized()
@@ -45,6 +48,8 @@ func _physics_process(_delta : float) -> void:
 			current_path = []
 	
 func move_to_point() -> void:
+	if target == null:
+		return
 	var start_cell : Vector2i = floor_map.tilemap.local_to_map(global_position)
 	var path : Array[Vector2i] = get_astar_path(start_cell, target)
 	print(start_cell, "\n", path)
@@ -68,3 +73,33 @@ func get_astar_path(start : Vector2i, end : Vector2i) -> Array[Vector2i]:
 		tile_path.append(floor_map.id_to_tile(id_cell))
 	
 	return tile_path
+
+func npc_action() -> void:
+	set_physics_process(false)
+	match current_action:
+		action.GET_POTION:
+			# check shelf for potion
+			var roll : int = randi_range(1,6)
+			if roll > 2:
+				current_action = action.CHECKOUT
+				target = checkout
+				move_to_point()
+			else:
+				if shelves.is_empty():	# no more shelves to visit
+					current_action = action.LEAVE
+					target = floor_map.spawn
+					move_to_point()
+				else:
+					var next_shelf : int = randi_range(0, len(shelves) - 1)
+					target = shelves.pop_at(next_shelf)
+					move_to_point()
+		action.CHECKOUT:
+			print("NPC checked out")
+			current_action = action.LEAVE
+			target = floor_map.spawn
+			move_to_point()
+		action.LEAVE:
+			print("MPC left")
+			queue_free()
+	await get_tree().create_timer(5.0).timeout
+	set_physics_process(true)
