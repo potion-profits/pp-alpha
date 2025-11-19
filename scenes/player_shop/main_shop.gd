b6270aa19ba7f5c463944b54a3d5c4ceee448e76
Connor-Villanueva <ckvillan@calpoly.edu>
2025-11-18 18:55:39 -0800

    Changed Y-Sorting & Crate Collisions

--- scenes/player_shop/main_shop.gd
+++ scenes/player_shop/main_shop.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @onready var frontroom_backdoor_dest_marker: Marker2D = $frontroom_backdoor_dest_marker
 @onready var backroom_frontdoor_dest_marker: Marker2D = $backdoor_frontroom_dest_marker
-@onready var player_camera: Camera2D = $Objects/Player/Camera2D
+@onready var player_camera: Camera2D = $EntityManager/Player/Camera2D
 
 func _ready()->void:
 	var pause_scene : Resource = preload("res://scenes/ui/pause_menu.tscn")
