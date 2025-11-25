5c6df4b7ed8f0e65c72a6e9af17cea16f39b29b3
Ozcar Cayetano <olopez2015.ol@gmail.com>
2025-11-25 14:43:57 -0800

    cash register flow works, need to check gold actually transfered

--- scenes/cash_register/cash_register.gd
+++ scenes/cash_register/cash_register.gd
@@ -1,12 +1,32 @@
 extends Node2D
 
-var player_in : bool = false
-var npc_ready : bool = false
+var player: Player
+var curr_npc : Npc
 
 
-func _on_interactable_area_body_entered(body: Node2D) -> void:
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("interact") and curr_npc and player and not curr_npc.is_checked_out:
+		curr_npc.is_checked_out = true
+		curr_npc.checkout_timer.timeout.emit()
+		print("YOU SOLD SOMETHING!!!!!\n")
+		player.set_chips(50)
+
+
+func _on_interactable_body_entered(body: Node2D) -> void:
+	print(body.name)
 	if body is Player:
-		player_in = true
-	if body is Npc and body.current_action == :
-		
-	pass # Replace with function body.
+		print("Player in")
+		player = body
+	if body is Npc and body.current_action == body.action.CHECKOUT and not body.is_checked_out:
+		print("NPC in")
+		curr_npc = body
+
+
+func _on_interactable_body_exited(body: Node2D) -> void:
+	print(body.name)
+	if body is Player:
+		print("Player out")
+		player = null
+	if body is Npc and body.current_action == body.action.LEAVE:
+		print("NPC out")
+		curr_npc = null
