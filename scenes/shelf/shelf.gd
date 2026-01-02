d65350681d0edc8136e09574021befb4cd1cc10a
elijah-villanu <elijahred9@outlook.com>
2026-01-02 12:40:24 -0800

    Remove Init Clear

--- scenes/shelf/shelf.gd
+++ scenes/shelf/shelf.gd
@@ -40,8 +40,6 @@ func _ready()-> void:
 	update_visuals()
 
 func init_visuals()->void:
-	potion_visuals.clear()
-	fill_visuals.clear()
 	potion_visuals.resize(inv_size)
 	fill_visuals.resize(inv_size)
 	
