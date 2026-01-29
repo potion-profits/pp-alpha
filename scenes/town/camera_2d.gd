5f0fdd10ab60825c32f40e034444f01a5005b46d
jmgdls <jazzymae.delossantos@gmail.com>
2026-01-29 08:37:25 -0800

    town scenes and camera script

--- scenes/town/camera_2d.gd
+++ scenes/town/camera_2d.gd
@@ -2,6 +2,6 @@ extends Camera2D
 
 @export var target: Node2D
 
-func _process(_delta) -> void:
+func _process(_delta: float) -> void:
 	if target:
 		global_position = target.global_position + Vector2(0, -50)
