2ad0824b5d4f49fd17d0567add6f335485ca4adc
elijah-villanu <elijahred9@outlook.com>
2026-01-29 09:32:28 -0800

    Cleanup

--- globals/music_manager.gd
+++ globals/music_manager.gd
@@ -24,7 +24,7 @@ var silent_db: float = -70.0
 ## Time for crossfade
 var fade_time: float = 2.0
 ## Volume level for music
-var music_db: float = -20.0
+var full_volume_db: float = -20.0
 
 func _ready() -> void:
 	current_song = song_contexts.get("res://scenes/ui/start_menu.tscn")
@@ -49,7 +49,7 @@ func play_bg_music(scene_path: String) -> void:
 func transition_song(new_song: AudioStreamPlayer) -> void:
 	if !current_song:
 		current_song = new_song
-		current_song.volume_db = 0
+		current_song.volume_db = full_volume_db
 		current_song.play()
 		return
 
@@ -68,7 +68,7 @@ func transition_song(new_song: AudioStreamPlayer) -> void:
 	# allows both the new and old song to fade concurrently
 	crossfade.set_parallel(true)
 	crossfade.tween_property(old_song, "volume_db", silent_db, fade_time)
-	crossfade.tween_property(new_song, "volume_db", music_db, fade_time)
+	crossfade.tween_property(new_song, "volume_db", full_volume_db, fade_time)
 
 	# tween signal once crossfade is finished
 	crossfade.finished.connect(on_crossfade_finish)
@@ -77,4 +77,4 @@ func transition_song(new_song: AudioStreamPlayer) -> void:
 func on_crossfade_finish() -> void:
 	if old_song:
 		old_song.stop()
-		old_song.volume_db = 0.0
+		old_song.volume_db = full_volume_db
