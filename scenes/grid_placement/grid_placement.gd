420a9dff6882203fca46dfecda0ed9479327d7ef
Ozcar Cayetano <olopez2015.ol@gmail.com>
2026-02-12 15:56:43 -0800

    comments and some clean up

--- scenes/grid_placement/grid_placement.gd
+++ scenes/grid_placement/grid_placement.gd
@@ -1,39 +1,59 @@
 extends Node2D
 
+## Reference to player character
 @onready var player: Player = $Player
+## Reference to static camera in front room
 @onready var front_cam: Camera2D = $FrontRoom/FrontCam
+## Reference to static camera in back room
 @onready var back_cam: Camera2D = $BackRoom/BackCam
+## Reference to the layer containing all placed entities
 @onready var entity_layer: TileMapLayer = $EntityLayer
+## Reference to the layer continaing the previewed placement
 @onready var preview_layer: TileMapLayer = $PreviewLayer
+## Reference to the floors in the back room
 @onready var back_floor: TileMapLayer = $BackRoom/BackroomFloors
+## Reference to the floors in the front room
 @onready var front_floor: TileMapLayer = $FrontRoom/Floor
+## Reference to the checkout counter tiles
 @onready var counters: TileMapLayer = $FrontRoom/Counters
+## Reference to the entity manager 
 @onready var em: EntityManager = $EntityManager
+## Reference to the price label
 @onready var cost: Label = $Static_UI/HBoxContainer/Cost
+## Reference to the smooth movement handler
 @onready var hold_controller: DirectionalHoldController = $DirectionalHoldController
 
+## Holds the position of the currently hovered tile
 var current_tile: Vector2i = Vector2i.ZERO
+## Holds the floor of the current room accessed
 var current_layer:TileMapLayer = front_floor
 
-var topleft : Vector2i
-var botright : Vector2i
+var topleft : Vector2i	## The top left corner of the floor for bounds checking
+var botright : Vector2i	## The bottom right corner for bounds checking
 
-const SHELF_IDX = 3
+const SHELF_IDX = 3	## Holds a constant refence to the shelf
 
+## Holds the index of the currently cycled entity
 var current_entity_idx: int = SHELF_IDX
+## Order of all entities used for cycling
 const ENTITY_ORDER: Array = ["barrel", "crate", "cauldron", "shelf"]
+## Holds the code of the currently selected entity
 var current_entity: String = ENTITY_ORDER[current_entity_idx]
 
+## Flag to allow placement only after scene is done loading
 var done_loading:bool = false
 
-
+## Holds the position of the last tile
 var _last_tile : Vector2i = Vector2i(-9999, -9999)
+## Holds the previous entity's code
 var _last_entity : String = ""
+## Holds the previous valid flag
 var _last_valid : bool = false
 
-
+## The scene number for an invalid placement block
 const BLOCK_ID: int = 6
 
+## Holds the information of each placable entities
 const ENTITIES : Dictionary = {
 	"barrel": {
 		"tile_id": 1,
@@ -62,23 +82,33 @@ const ENTITIES : Dictionary = {
 	},
 }
 
+## The tileset source id
 const TILESET_SOURCE : int = 0
+## Used to check if a tile is empty
 const EMPTY_TILE : int = -1
+## Used to modulate on invalid placement
 const INVALID_COLOR : Color = Color("Red")
-const VALID_COLOR : Color = Color(1,1,1,1)
+## Used to modulate on valid placement
+const VALID_COLOR : Color = Color("White")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player.set_physics_process(false) # need gold but dont want to move charactser
+	# Connects the movement signal from controller to on step
 	hold_controller.stepped.connect(_on_step)
 	
+	# default to front room
 	front_cam.make_current()
 	current_layer = front_floor
 	_update_bounds()
+	
+	# default to top left tile
 	current_tile = topleft
 	update_price()
 	
+	# wait a frame so the entities load properly
 	await get_tree().process_frame
+	# places the entities on the proper tile
 	_restore_entities_to_tilemap()
 
 # Switches to and from back and front room
@@ -89,17 +119,18 @@ func _switch_cam() -> void:
 		switch_to_front()
 	reset_cursor()
 
-
+## Removes the current preview tile and places the cursor at the top left
 func reset_cursor() -> void:
 	preview_layer.erase_cell(current_tile)
 	current_tile = topleft
 
-
+## Switch to the back room
 func switch_to_back() -> void:
 	back_cam.make_current()
 	current_layer = back_floor
 	_update_bounds()
 
+## Switch to the front room
 func switch_to_front() -> void:
 	front_cam.make_current()
 	current_layer = front_floor
@@ -108,7 +139,7 @@ func switch_to_front() -> void:
 	update_price()
 	_update_bounds()
 	
-
+# update the top left and bottom right bounds
 func _update_bounds() -> void:
 	var tiles : Array = current_layer.get_used_cells()
 	topleft = tiles[0]
@@ -119,6 +150,7 @@ func _update_bounds() -> void:
 		botright = Vector2i(max(botright.x, tile.x), max(botright.y, tile.y))
 
 
+## moves to the tile in the given direction delta
 func move_cursor(dir_delta : Vector2i) -> void:
 	var next : Vector2i = current_tile + dir_delta
 	if next.x < topleft.x or next.x > botright.x:
@@ -127,6 +159,7 @@ func move_cursor(dir_delta : Vector2i) -> void:
 		return
 	current_tile = next
 
+# When a movement signal is sent, move the cursor
 func _on_step(dir: DirectionalHoldController.Direction)->void:
 	move_cursor(hold_controller.get_vector(dir))
 
@@ -146,7 +179,7 @@ func _input(event: InputEvent) -> void:
 		
 	hold_controller.handle_movement_input(event)
 
-
+# place entity and subtract money
 func _place() -> void:
 	if not done_loading:
 		return
@@ -157,6 +190,7 @@ func _place() -> void:
 	place_entity(current_tile)
 	player.set_coins(-(entity_info().price))
 
+## Moves the preview layer entity to the entity layer
 func place_entity(tile: Vector2i) -> void:
 	entity_layer.set_cell(tile, TILESET_SOURCE, Vector2i.ZERO, entity_info().tile_id)
 	
@@ -165,6 +199,7 @@ func place_entity(tile: Vector2i) -> void:
 	preview_layer.erase_cell(tile)
 	_save_tile()
 
+# cycle through the possible entities
 func _cycle() -> void:
 	if current_layer == front_floor:
 		current_entity_idx = SHELF_IDX
@@ -174,57 +209,69 @@ func _cycle() -> void:
 	current_entity = ENTITY_ORDER[current_entity_idx]
 	update_price()
 
+# change the price label to reflect the current entity's price
 func update_price()->void:
 	cost.text = str(-entity_info().price)
 
+# every frame, try to update the preview and process any movement
 func _process(delta: float) -> void:
 	update_preview()
 	hold_controller.process(delta)
 
+## Updates the preview layer to show on correct tile and color based on validity
 func update_preview()->void:
+	# check if tile is free to place
 	var valid : bool = can_place_entity(current_tile)
 	
 	if _tile_state_unchanged(valid):
 		return
 	
+	# ensure nothing else is on the preview
 	preview_layer.clear()
-	draw_preview_tile(current_tile, valid)
-	_update_tile_state(valid)
+	draw_preview_tile(current_tile, valid) # draw the tile
+	_update_tile_state(valid)	#update its state
 
+# check the state of the tile
 func _tile_state_unchanged(valid : bool) -> bool:
 	return current_tile == _last_tile and current_entity == _last_entity and valid == _last_valid
 
+# update the state of the tile
 func _update_tile_state(valid : bool) -> void:
 	_last_entity = current_entity
 	_last_tile = current_tile
 	_last_valid = valid
 
+## Draw the current entity on the current tile of the preview layer
 func draw_preview_tile(tile: Vector2i, valid: bool) -> void:
 	preview_layer.set_cell(tile, TILESET_SOURCE, Vector2i.ZERO, entity_info().tile_id)
 	preview_layer.modulate = VALID_COLOR if valid else INVALID_COLOR
 
+## Commits the preview layer entity into the entity manager at the current location
 func _save_tile() -> void:
 	var local_coords: Vector2 = preview_layer.map_to_local(current_tile)
 	var placed_entity:Entity = em.create_entity(current_entity)
 	placed_entity.position = local_coords
 	em.add_child(placed_entity)
 
+# Returns true if the given tile on the given layer is empty
 func _empty_cell(layer: TileMapLayer, coords: Vector2i) -> bool:
 	return layer.get_cell_source_id(coords) == EMPTY_TILE
 	
+# Goes through all entities and draws them on a tilemap
 func _restore_entities_to_tilemap()->void:
 	for entity in em.get_children():
 		if entity.is_in_group("entity"):
 			_restore_tile(entity.entity_code, entity_layer.local_to_map(entity.position))
 	done_loading = true
 
+# Draws the given entity onto the given tile 
 func _restore_tile(entity_code:String, tile: Vector2i)->void:
 	entity_layer.set_cell(tile, TILESET_SOURCE, Vector2i.ZERO, ENTITIES[entity_code].tile_id)
 	
 	if entity_code == "shelf" or entity_code == "bed":
 		entity_layer.set_cell(tile + Vector2i.UP, TILESET_SOURCE, Vector2i.ZERO, BLOCK_ID)
 	
-
+## Returns true if tbe given tile can have something placed on it
 func can_place_entity(tile : Vector2i) -> bool:
 	if not _empty_cell(entity_layer, tile):
 		return false
@@ -242,9 +289,10 @@ func can_place_entity(tile : Vector2i) -> bool:
 	
 	return true
 
+# returns to menu, should change to shop when merged
 func _menu()->void:
 	SceneManager.change_to("res://scenes/town_menu/town_menu.tscn")
 	
-
+## Returns the info of the current entity from ENTITIES
 func entity_info() -> Dictionary:
 	return ENTITIES[current_entity]
--- scenes/refill_scene/backroom.gd
+++ scenes/refill_scene/backroom.gd
@@ -10,43 +10,64 @@ extends Node2D
 @onready var em: EntityManager = $EntityManager	## Reference to entity manager
 @onready var player: Player = $EntityManager/Player	## Reference to player
 @onready var static_ui: CanvasLayer = $Static_UI	## reference to static ui
-@onready var hold_controller: DirectionalHoldController = $DirectionalHoldController
+## Reference to smooth movement handler
+@onready var hold_controller: DirectionalHoldController = $DirectionalHoldController 
+## Reference to the tile map layer that 
 @onready var current_layer: TileMapLayer = $BackRoom/BackroomFloors
+## Reference to the price indicator on the UI
 @onready var cost: Label = $Static_UI/HBoxContainer/Cost
+## Reference to the texture preview on the UI
 @onready var blank_texture: TextureRect = $Static_UI/HBoxContainer/BlankTexture
 
-var current_entity : Entity
 
-var selectables : Array[Entity]
+var current_entity : Entity ## The entity that is currently selected
+var selectables : Array[Entity] ## Holds all the selectable entities in the scene
 
+## Holds the unique entity codes for all the selectables
 const SELECTABLE : Array = ["crate", "barrel"]
+## Default price for saftey
 const FALLBACK_PRICE : int = 500
+## Has the prices for each type of refill
 const SELECTABLE_PRICES : Dictionary = {
-	"empty_barrel":INF,
 	"red_barrel":160,
 	"blue_barrel": 250,
 	"green_barrel": 340,
 	"dark_barrel": 420,
 	"crate": 80
 }
+## Maintains a consistent order of barrel types to cycle through
 const BARREL_ORDER: Array = ["red_barrel", "blue_barrel", "green_barrel", "dark_barrel"]
+
+## Holds the index of the currently cycled through barrel refill type; default to red
 var current_barrel_idx : int = 0
 
 func _ready()->void:
 	player.set_physics_process(false) # need gold but dont want to move character
-	hold_controller.stepped.connect(_on_step)
-	await get_tree().process_frame
-	_find_init_target()
-	_highlight_current()
-	update_price()
 	
-# gets all the entities that are being targetted (barrels or crates)
+	## Connects the on step function to the movement handler
+	hold_controller.stepped.connect(_on_step)
+	
+	## Needs to first load all the entities to be able to highlight them, 
+	## so we wait a frame
+	await get_tree().process_frame
+	
+	## get the initial entity (just the first one to be found)
+	_find_init_target()
+	## highlight the initial entity
+	_highlight_current()
+	## make UI reflect the selected highlight's refill
+	update_ui()
+	
+# gets all the entities that are being targetted (barrels or crates) and 
+# picks the first valid one to be the current target
 func _find_init_target() -> void:
 	if em:
 		for child in em.get_children():
 			if child is Entity and child.entity_code in SELECTABLE:
+				# only set current_entity if one hasnt been set
 				if not current_entity:
 					current_entity = child as Entity
+				# always append to selectables list
 				selectables.append(child)
 
 ## Calls the refill function on the selected target and reduces player coins by cost.
@@ -55,32 +76,40 @@ func refill()->void:
 	var price : int =  _get_cost()
 	var coins : int = player.get_coins()
 	
+	# When cant afford, text goes red for 0.3s and then white again
 	if coins < price:
 		cost.modulate = Color("Red")
 		await get_tree().create_timer(0.3).timeout
 		cost.modulate = Color("White")
 		return
-		
-	player.set_coins(-price)
 	
+	# When can afford, deduce price and refill target
+	player.set_coins(-price)
+
 	if current_entity.entity_code == "barrel":
 		current_entity.refill(BARREL_ORDER[current_barrel_idx])
 	else:
 		current_entity.refill()
 
-
+# changes the barrel selection index
 func _cycle() -> void:
-	current_barrel_idx = (current_barrel_idx + 1) % len(BARREL_ORDER)
-	update_price()
+	# only cycle if currently on a barrel
+	if current_entity.entity_code == "barrel":
+		current_barrel_idx = (current_barrel_idx + 1) % len(BARREL_ORDER)
+		update_ui() # updates ui accordingly
 
+# returns the cost of the current entity
 func _get_cost() -> int:
 	var e_type: String = current_entity.entity_code
 	if e_type == "crate":
 		return SELECTABLE_PRICES.get(e_type, FALLBACK_PRICE)
 	return SELECTABLE_PRICES.get(BARREL_ORDER[current_barrel_idx], FALLBACK_PRICE)
 
-func update_price()->void:
-	cost.text = str(-_get_cost())
+## Updates UI to reflect the current entity
+func update_ui()->void:
+	cost.text = str(-_get_cost()) # change the cost label
+	
+	# change the icon
 	if current_entity.entity_code == "barrel":
 		blank_texture.texture = current_entity.get_barrel_texture(BARREL_ORDER[current_barrel_idx])
 	else:
@@ -88,17 +117,23 @@ func update_price()->void:
 		atlas.atlas = current_entity.full_crate.texture
 		atlas.region = current_entity.full_crate.region_rect
 		blank_texture.texture = atlas
-		
+
+# Finds the closest selectable in the given direction and sets it to current
 func find_next_selectable(dir: Vector2) -> Entity:
 	var best_entity: Entity = current_entity
 	var min_dist : float = INF
-	var from : Vector2 = current_layer.local_to_map(best_entity.position)
+	var from : Vector2 = best_entity.position
+	
+	# calculates distance to each selectables
 	for entity : Entity in selectables:
-		var pos : Vector2 = current_layer.local_to_map(entity.position)
+		var pos : Vector2 = entity.position
 		var to_candidate : Vector2 = pos - from
+		
+		# if its the current, dont check
 		if to_candidate == Vector2.ZERO:
 			continue
 		
+		# check that its in the correct direction
 		if dir.x > 0 and to_candidate.x <= 0:
 			continue
 		if dir.x < 0 and to_candidate.x >= 0:
@@ -108,11 +143,13 @@ func find_next_selectable(dir: Vector2) -> Entity:
 		if dir.y < 0 and to_candidate.y >= 0:
 			continue
 		
+		# prioritizes entities that are more on the x or y based on the given direction
 		if dir.x != 0 and abs(to_candidate.y) > abs(to_candidate.x):
 			continue
 		if dir.y != 0 and abs(to_candidate.x) > abs(to_candidate.y):
 			continue
 		
+		# gets the minimum of previous and current
 		var dist := to_candidate.length()
 		if dist < min_dist:
 			min_dist = dist
@@ -120,23 +157,35 @@ func find_next_selectable(dir: Vector2) -> Entity:
 	
 	return best_entity
 
+# When direction signals a step, handle the step by moving the highlight in the
+# given directions.
 func _on_step(dir: DirectionalHoldController.Direction)->void:
+	# Get the held direction
 	var delta : Vector2i = DirectionalHoldController.DIR_VECTORS.get(dir)
+	
+	# if no direction, don't do anything
 	if delta == null:
 		return
 	
+	# get the next entity in the given direction
 	var next_entity : Entity = find_next_selectable(delta)
+	
+	# if the closest is the current, dont change
 	if next_entity == current_entity:
 		return
 	
+	# change the highlight
 	_unhighlight_current()
 	current_entity = next_entity
 	_highlight_current()
-	update_price()
+	# update ui to new entity
+	update_ui()
 	
+# removes the highlight from the current entity
 func _unhighlight_current()->void:
 	current_entity.un_highlight()
 
+# highlights the current entity
 func _highlight_current()->void:
 	current_entity.highlight()
 
@@ -156,6 +205,7 @@ func _input(event: InputEvent) -> void:
 	
 	hold_controller.handle_movement_input(event)
 
+# on every frame, process the hold_controller
 func _process(delta: float) -> void:
 	hold_controller.process(delta)
 
