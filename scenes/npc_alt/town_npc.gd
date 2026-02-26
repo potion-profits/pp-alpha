997ea1075f47e6fca5b7625c88085def8d8bd480
Ozcar Cayetano <olopez2015.ol@gmail.com>
2026-02-26 08:57:23 -0800

    Npcs spawn

--- scenes/npc_alt/town_npc.gd
+++ scenes/npc_alt/town_npc.gd
@@ -2,12 +2,47 @@ class_name TownNpc extends Npc
 
 ## This class represents an NPC that will spawn within the town scene
 
+
+@onready var wait_time: Timer = $wait_time
+
 ## Represents the last direction of the NPC
 var last_dir : String = "up"
+var floor_map : Node2D
+var target: Vector2
 
+var is_moving : bool = false
+
+## Represents the astar path of the NPC
+var current_path : Array = []
+## Represents the current index of the NPC's path
+var path_index : int = 0
+
+const SPEED : int = 100
+const roam_min : float = 5
+const roam_max : float = 10
+
+var time_elapsed : float = 0
+var roam_timeout : float = randf_range(roam_min, roam_max)
 
 func _ready() -> void:
 	super._ready()
+
+func rand_move() -> void:
+	var off1 : int = randi_range(-100,100)
+	var off2 : int = randi_range(-100,100)
+	target = self.position + Vector2(off1, off2)
+
+func _process(delta: float) -> void:
+	time_elapsed += delta
+	
+	if time_elapsed >= roam_timeout and not is_moving:
+		is_moving = true
+		rand_move()
+		roam_timeout = randf_range(roam_min, roam_max)
+		move_to_point()
+		roam_timeout = randf_range(roam_min, roam_max)
+		time_elapsed = 0
+		is_moving = false
 
 ## Animates the NPC based on velocity determined by movement along path
 func animate(x_dir: float, y_dir : float) -> void:
@@ -39,3 +74,9 @@ func animate(x_dir: float, y_dir : float) -> void:
 	else:
 		if sprite.sprite_frames.has_animation("idle_" + last_dir):
 			sprite.play("idle_" + last_dir)
+
+func move_to_point() -> void:
+	if target == null:
+		return
+	velocity = (target - self.position).normalized() * SPEED
+	move_and_slide()
--- scenes/town/town.gd
+++ scenes/town/town.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+var spawn_location_pos : Array = []
+@onready var spawn_locations: Node2D = $SpawnLocations
+@onready var npcs: Node2D = $NPCs
+
+const town_npc_scene : PackedScene = preload("res://scenes/npc_alt/town_npc.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,8 +12,22 @@ func _ready() -> void:
 	var menu_instance : Node = pause_scene.instantiate()
 	add_child(menu_instance)
 	GameManager.set_pause_menu(menu_instance.get_node("PauseMenuControl"))
+	for location : Marker2D in spawn_locations.get_children():
+		spawn_location_pos.append(location.position)
+	spawn_town_npcs()
 
+func spawn_npc(loc: Vector2) -> void:
+	var t_npc : TownNpc = town_npc_scene.instantiate()
+	t_npc.position = loc
+	npcs.add_child(t_npc)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func spawn_town_npcs()->void:
+	for location : Vector2 in spawn_location_pos:
+		location = squib(location)
+		spawn_npc(location)
+
+func squib(loc : Vector2) -> Vector2:
+	var off1 : int = randi_range(0,10)
+	var off2 : int = randi_range(0,10)
+	
+	return loc + Vector2(off1, off2)
