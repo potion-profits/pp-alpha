0d5ddddcd30e1b3a00ed683a328efd88f200d9b0
elijah-villanu <elijahred9@outlook.com>
2026-02-26 09:27:57 -0800

    Add Start Menu Transition

--- scenes/cinematics/opening_logos.gd
+++ scenes/cinematics/opening_logos.gd
@@ -3,16 +3,16 @@ extends Control
 @onready var animation: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
-	if OS.is_debug_build():
+	if !OS.is_debug_build():
 		play_logos()
-	
 
 # Plays logos (Godot Engine, Studio Logo)
 func play_logos() -> void:
 	if animation:
 		animation.play("fade_in")
 		# time for logo to appear
-		await get_tree().create_timer(6.0).timeout
+		await get_tree().create_timer(4.5).timeout
+		
 		animation.play("fade_out")
 		# this timer must match fade out time to ensure scene switch after fade is finished
 		await get_tree().create_timer(3.0).timeout
--- scenes/ui/start_menu.gd
+++ scenes/ui/start_menu.gd
@@ -4,14 +4,21 @@ extends "res://scenes/ui/base_menu.gd"
 ##
 ## Has the ability to play the game, open the options, and quit/close the game.
 
+# Reference to animation player that contains the fade in transition
+@onready var animation: AnimationPlayer = $FadeIn
+# Reference to rectangle used for animations
+@onready var fade_color: ColorRect = $ColorRect
+
 func _ready()->void:
+	# fade in the start menu (Need to discuss: fade on all switches to start menu?)
+	if !OS.is_debug_build():
+		play_fade_in()
 	button_map = {
 		"MarginContainer/VBoxContainer/Play": "res://assets/ui/play_button.tres",
 		"MarginContainer/VBoxContainer/Options": "res://assets/ui/options_button.tres",
 		"MarginContainer/VBoxContainer/Quit": "res://assets/ui/quit_button.tres"
 	}
 	super._ready()
-
 
 func _on_play_pressed()->void:
 	SceneManager.change_to("res://scenes/player_shop/main_shop.tscn")
@@ -23,3 +30,9 @@ func _on_quit_pressed()->void:
 	#we will hard save here for now
 	#GameManager.commit_to_storage()
 	get_tree().quit()
+
+func play_fade_in() -> void:
+	animation.play("fade_in")
+	# remove color rect node to not block inputs once fade is finished
+	await get_tree().create_timer(4.0).timeout
+	fade_color.queue_free()
