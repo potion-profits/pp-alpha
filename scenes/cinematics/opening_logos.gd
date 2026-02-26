70e07bfaa5b37a12cc70e7406694e28a492c4567
elijah-villanu <elijahred9@outlook.com>
2026-02-26 09:55:52 -0800

    Add Studio Logo

--- scenes/cinematics/opening_logos.gd
+++ scenes/cinematics/opening_logos.gd
@@ -1,6 +1,9 @@
 extends Control
 
 @onready var animation: AnimationPlayer = $AnimationPlayer
+@onready var shay : AnimatedSprite2D = $Shay
+@onready var godot_logo: Sprite2D = $GodotLogo
+@onready var studio_logo: Sprite2D = $StudioLogo
 
 func _ready() -> void:
 	if !OS.is_debug_build():
@@ -9,12 +12,25 @@ func _ready() -> void:
 # Plays logos (Godot Engine, Studio Logo)
 func play_logos() -> void:
 	if animation:
+		# Play Godot Logo
 		animation.play("fade_in")
 		# time for logo to appear
 		await get_tree().create_timer(4.5).timeout
-		
 		animation.play("fade_out")
 		# this timer must match fade out time to ensure scene switch after fade is finished
 		await get_tree().create_timer(3.0).timeout
+		
+		# switch logo states
+		godot_logo.visible = false
+		studio_logo.visible = true
+		shay.visible = true
+		
+		# Play studio logo
+		shay.play("walk_left")
+		animation.play("fade_in")
+		await get_tree().create_timer(4.5).timeout
+		animation.play("fade_out")
+		await get_tree().create_timer(3.0).timeout
+		
 		# Switch to main menu 
 		SceneManager.change_to("res://scenes/ui/start_menu.tscn")
