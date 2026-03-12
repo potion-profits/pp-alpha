6d9c44979ff9bb552d13559aca6ad493055f2528
elijah-villanu <elijahred9@outlook.com>
2026-03-11 17:22:44 -0700

    SFX Manager Abstraction + Adjust Mix
    Additionally changed the window stretch mode to accomodate responsive windows (aspect ratio always locked). This change made by Ozcar will probably be in main by the time this gets merged in

--- globals/sfx_manager.gd
+++ globals/sfx_manager.gd
@@ -3,12 +3,43 @@ extends Node
 
 ## Handles non-entity and global sound effects (ex: user clicks, claudron ambience, all scene ambience, time specific sfx)
 
+## References to SFX related sounds
+@onready var alarm_sfx: AudioStreamPlayer = $AlarmSFX
+@onready var click_sfx: AudioStreamPlayer = $ClickSFX
+@onready var shelf_click_sfx: AudioStreamPlayer = $AlarmSFX
+@onready var workday_end_sfx: AudioStreamPlayer = $WorkdayEndSFX
+@onready var sfx_directory: Dictionary = {
+	"alarm": alarm_sfx,
+	"click": click_sfx,
+	"shelf_click": shelf_click_sfx,
+	"workday_end": workday_end_sfx
+}
+
+## References to dialouge related sounds
+@onready var cat_dialouge: AudioStreamPlayer = $CatDialouge
+@onready var npc_dialouge: AudioStreamPlayer = $NPCDialouge
+@onready var shark_dialouge: AudioStreamPlayer = $SharkDialouge
+@onready var dialouge_directory: Dictionary = {
+	"cat": cat_dialouge,
+	"shark": shark_dialouge,
+	"npc": npc_dialouge
+}
+
+## References to ambient related sounds
+@onready var casino_ambience: AudioStreamPlayer = $CasinoAmbience
+@onready var town_ambience: AudioStreamPlayer = $TownAmbience
+@onready var ambience_directory: Dictionary = {
+	"casino": casino_ambience,
+	"town": town_ambience
+}
+
+
 # Reference to each cauldron's ambience audio node
 var cauldron_players: Array[AudioStreamPlayer2D] = []
 var cauldron_muted: bool = false
 
-#func _ready() -> void:
-	#TimeManager.workday_end.connect()
+func _ready() -> void:
+	handle_signals()
 
 # Only the first cauldon registered will play, the backroom seems small enough to not
 # Distinguish that only one is playing
@@ -42,3 +73,22 @@ func mute_ambience() -> void:
 
 func unmute_ambience() -> void:
 	cauldron_muted = false
+
+func play_sfx(sound_name: String) -> void:
+	var sound_to_play: AudioStreamPlayer = sfx_directory.get(sound_name)
+	if sound_to_play:
+		if !sound_to_play.playing:
+			sound_to_play.play()
+		else:
+			sound_to_play.stop()
+
+## Dialouge sound effects is simply a 40-50 second stream that plays 2-4 seconds
+## proportional to text 
+#func play_dialouge(dialouge_name: String, dialouge: String) -> void:
+	## function that determines how long a sound should play (temp)
+	#var how_long_to_play: int = 3
+	
+
+func handle_signals() -> void:
+	TimeManager.workday_start.connect(play_sfx.bind("alarm"))
+	TimeManager.workday_end.connect(play_sfx.bind("workday_end"))
