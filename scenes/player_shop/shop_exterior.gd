976051c365580f68b05405e0ee5b6edd22513531
Connor-Villanueva <ckvillan@calpoly.edu>
2026-03-11 21:46:31 -0700

    Added tooltips to exterior shops

--- scenes/player_shop/shop_exterior.gd
+++ scenes/player_shop/shop_exterior.gd
@@ -4,12 +4,12 @@ extends Entity
 
 @onready var interactable: Area2D = $Interactable
 
-var interact_key: String = InputMap.get_action_description("interact").split(" ")[0]
-var DOOR_TOOLTIP: String = "Press %s to Enter" %[interact_key]
+var player_in_area: Player
+var DOOR_TOOLTIP: String = "Press %s to Enter"
 
 func _ready() -> void:
 	interactable.interact = _on_interact
-	interactable.tooltip = DOOR_TOOLTIP
+	interactable.set_tooltip_label(DOOR_TOOLTIP)
 	
 	super._ready()
 	entity_code = "player_shop_ext"
@@ -17,3 +17,19 @@ func _ready() -> void:
 ## Transitions to the main shop scene on interaction.
 func _on_interact() -> void:
 	SceneManager.change_to("res://scenes/player_shop/main_shop.tscn")
+
+
+func _on_interactable_body_entered(body: Node2D) -> void:
+	if body is Player:
+		player_in_area = body
+		set_process(true)
+
+
+func _on_interactable_body_exited(body: Node2D) -> void:
+	if body is Player:
+		player_in_area = null
+		set_process(false)
+
+func _process(_delta: float) -> void:
+	if player_in_area:
+		interactable.set_tooltip_label(DOOR_TOOLTIP)
