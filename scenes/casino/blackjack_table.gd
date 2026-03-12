b446cdcdf8f1e9994b4c8a7640f453585239cce3
Connor-Villanueva <ckvillan@calpoly.edu>
2026-03-11 21:44:28 -0700

    Added tooltips to casino and cashier NPCs

--- scenes/casino/blackjack_table.gd
+++ scenes/casino/blackjack_table.gd
@@ -3,13 +3,13 @@ extends Entity
 @onready var interactable : Area2D = $Interactable
 @onready var table_sprite: Sprite2D = $TableSprite
 
-var interact_key: String = InputMap.get_action_description("interact").split(" ")[0]
-var BLACKJACK_TOOLTIP: String = "Press %s to Play Blackjack" %[interact_key]
+var player_in_area: Player
+var BLACKJACK_TOOLTIP: String = "Press %s to Play Blackjack"
 
 func _ready() -> void:	
 	# Links interactable template to barrel specific method
 	interactable.interact = _on_interact
-	interactable.tooltip = BLACKJACK_TOOLTIP
+	interactable.is_interactable = false
 	
 	# Sets up entity info
 	super._ready()
@@ -21,3 +21,24 @@ func _on_interact() -> void:
 			# invalid chip amount to play blackjack
 			return
 		SceneManager.change_to("res://scenes/casino/black_jack.tscn")
+
+
+func _on_interactable_body_entered(body: Node2D) -> void:
+	if body is Player:
+		player_in_area = body
+		set_process(true)
+
+
+func _on_interactable_body_exited(body: Node2D) -> void:
+	if body is Player:
+		player_in_area = null
+		set_process(false)
+		
+func _process(_delta: float) -> void:
+	if player_in_area:
+		if (player_in_area.chips > 0):
+			interactable.set_tooltip_label(BLACKJACK_TOOLTIP)
+			interactable.is_interactable = true
+			return
+	
+	interactable.is_interactable = false
--- scenes/npc_alt/cashier_npc.gd
+++ scenes/npc_alt/cashier_npc.gd
@@ -6,9 +6,9 @@ extends Npc
 @onready var interactable: Area2D = $Interactable
 @export var is_interactable: bool = true
 
-var interact_key: String = InputMap.get_action_description("interact").split(" ")[0]
-var CASINO_CASHIER_TOOLTIP: String = "Press %s to Exchange Shit" %[interact_key]
-var SUPPLYSHOP_CASHIER_TOOLTIP: String = "Press %s to Buy Shit" %[interact_key]
+var player_in_area: Player
+var CASINO_CASHIER_TOOLTIP: String = "Press %s to Exchange Shit"
+var SUPPLYSHOP_CASHIER_TOOLTIP: String = "Press %s to Buy Shit"
 
 func _ready() -> void:
 	sprite.sprite_frames = build_sprite_frames(idle_sheet, null)
@@ -19,6 +19,26 @@ func _ready() -> void:
 	# Change tooltip depending on scene the NPC is in
 	var current_scene : Node = SceneManager.current_scene()	
 	if (current_scene.name == "CasinoFloor"):
-		interactable.tooltip = CASINO_CASHIER_TOOLTIP
+		interactable.set_tooltip_label(CASINO_CASHIER_TOOLTIP)
 	elif (current_scene.name == "SupplyShop"):
-		interactable.tooltip = SUPPLYSHOP_CASHIER_TOOLTIP
+		interactable.set_tooltip_label(SUPPLYSHOP_CASHIER_TOOLTIP)
+
+
+func _on_interactable_body_entered(body: Node2D) -> void:
+	if body is Player:
+		player_in_area = body
+		set_process(true)
+
+func _on_interactable_body_exited(body: Node2D) -> void:
+	if body is Player:
+		player_in_area = null
+		set_process(false)
+		
+func _process(_delta: float) -> void:
+	if player_in_area:
+		var current_scene : Node = SceneManager.current_scene()
+		
+		if (current_scene.name == "CasinoFloor"):
+			interactable.set_tooltip_label(CASINO_CASHIER_TOOLTIP)
+		elif (current_scene.name == "SupplyShop"):
+			interactable.set_tooltip_label(SUPPLYSHOP_CASHIER_TOOLTIP)
